92fa3652fe4232416d8c2f57777380b201d589b3
Paul K. Lynch <dramatichamster@gmail.com>
2024-03-16 22:16:18 +1100

    Setting up uni tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, configDefaults } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import webfontDownload from 'vite-plugin-webfont-dl'
 import path from 'path'
@@ -11,6 +11,18 @@ export default defineConfig({
       features: path.resolve(__dirname, './src/features'),
       global: path.resolve(__dirname, './src/global'),
       lib: path.resolve(__dirname, './src/lib')
+    }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['cobertura', 'html'],
+      exclude: [
+        ...configDefaults.exclude
+      ]
     }
   }
 })
