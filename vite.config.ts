4879be8e42d2f01725b05d774393da54809f65a9
Paul K. Lynch <dramatichamster@gmail.com>
2024-03-30 23:04:30 +1100

    Deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), webfontDownload()],
+  base: '/hycalc/',
   resolve: {
     alias: {
       features: path.resolve(__dirname, './src/features'),
