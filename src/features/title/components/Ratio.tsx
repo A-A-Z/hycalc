7f0679e5e02322acb6d725e379656288cdb4e3dd
Paul K. Lynch <dramatichamster@gmail.com>
2024-03-31 19:25:35 +1100

    Style updates

--- src/features/title/components/Ratio.tsx
+++ src/features/title/components/Ratio.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
+import clsx from 'clsx'
 
 interface RatioProps {
   value: number
@@ -6,29 +7,36 @@ interface RatioProps {
 
 export const Ratio = ({ value }: RatioProps): JSX.Element => {
   const [currentValue, setCurrentValue] = useState(value)
+  // const [isUpdating, setIsUpdatin] = useState(false)
 
   useEffect(() => {
     // Only proceed if currentValue does not equal the target value
     if (currentValue !== value) {
-      const difference = Math.abs(value - currentValue);
-      const delay = difference > 10 ? 20 : (90 - difference * 10);
+      const difference = Math.abs(value - currentValue)
+      const delay = difference > 10 ? 20 : (90 - difference * 10)
 
       const timer = setTimeout(() => {
-        setCurrentValue(prevValue => prevValue + (value > prevValue ? 1 : -1));
-      }, delay);
+        setCurrentValue(prevValue => prevValue + (value > prevValue ? 1 : -1))
+      }, delay)
 
       // Return a cleanup function that clears the timeout
-      return () => clearTimeout(timer);
+      return () => clearTimeout(timer)
     }
     // If currentValue equals the target value, no cleanup function is needed.
     // This effectively returns `undefined`, which is a valid `void` return type.
-  }, [value, currentValue]);
+  }, [value, currentValue])
+
+  const isUpdating = useMemo(() => currentValue !== value, [currentValue, value])
 
   return (
-    <div className="title__ratio" role="status">
+    <div className={clsx('title__ratio', isUpdating && 'title__ratio--updating')} role="status">
       <span aria-hidden="true">{currentValue}</span>
       <span aria-hidden="true">%</span>
-      <div role="status" className="visually-hidden" aria-live="polite">{`${value} percent on site`}</div>
+      <div
+        role="status"
+        className="visually-hidden"
+        aria-live="polite"
+      >{`${value} percent on site`}</div>
     </div>
   )
 }
