a5951a9ce267acd74ba2fef2c90dfd82dc43aeae
Paul K. Lynch <dramatichamster@gmail.com>
2024-06-16 18:17:10 +1000

    Keyboard support for custom weekday buttons

--- src/features/calendar/components/Headings.tsx
+++ src/features/calendar/components/Headings.tsx
@@ -1,7 +1,5 @@
-import { memo, useCallback } from 'react'
-// import { FaToggleOn, FaToggleOff } from 'react-icons/fa6'
+import { memo, useCallback, useRef } from 'react'
 import { format, startOfWeek, addDays, DATE_FORMATS } from 'lib/date'
-// import { useGridStatus } from 'features/status'
 import { useConfig } from 'features/config'
 import { ToggleButton } from 'features/button'
 import { useActiveWeekdays } from '../hooks/useActiveWeekdays'
@@ -18,9 +16,9 @@ const HeadingTitle = ({ date }: HeadingTitleProps): JSX.Element => (
 )
 
 const HeadingsSrc = (): JSX.Element => {
+  const btnRefs = useRef<(HTMLButtonElement | null)[]>([])
   const start = startOfWeek(new Date)
   const days = Array.from({ length: 7 }).map((_, index) => addDays(start, index))
-  // const { status: { isCustomMode } } = useGridStatus()
   const { config: { weekdays }, setConfig } = useConfig()
 
   const { isCustomMode, isActiveWeekday } = useActiveWeekdays()
@@ -36,7 +34,6 @@ const HeadingsSrc = (): JSX.Element => {
     } else {
       // weekday is in array, remove it
       newWeekdays.splice(index, 1)
-      console.log('remove', index, newWeekdays)
     }
 
     // update the weekdays config
@@ -45,15 +42,32 @@ const HeadingsSrc = (): JSX.Element => {
 
   let colIndex = 0
 
+  // for handling nav keys on the custom mode buttons
+  const handleBtnArrowKey = useCallback((key: string, index: number) => {
+    switch (key) {
+      case 'ArrowRight':
+        btnRefs.current[index + 1]?.focus()
+        break
+
+      case 'ArrowLeft':
+        if (index > 0) {
+          btnRefs.current[index - 1]?.focus()
+        }
+        break
+
+      case 'Home':
+        btnRefs.current[0]?.focus()
+        break
+
+      case 'End':
+        btnRefs.current[6]?.focus()
+
+    }
+  }, [btnRefs.current])
+
   return (
     <div className="week week--header" role="row">
       {days.map((headerDate, dayIndex) => {
-        // const isColActive = weekdays.some(day => day === dayIndex)
-
-        // if (!isColActive && !isCustomMode) {
-        //   // if inactive weekday and not in custom mode then don't show this heading
-        //   return null
-        // }
         const isColActive = isActiveWeekday(headerDate)
         if (!isCustomMode && !isColActive) {
           return null
@@ -73,10 +87,14 @@ const HeadingsSrc = (): JSX.Element => {
               aria-colindex={colIndex}
             >
               <ToggleButton
+                ref={el => { btnRefs.current[dayIndex] = el }}
+                tabIndex={dayIndex === 0 ? 0 : -1}
                 isActive={isColActive}
                 onClick={() => { toggleWeekday(dayIndex) }}
+                onKeyDown={e => handleBtnArrowKey(e.key, dayIndex)}
               >
                 <HeadingTitle date={headerDate} />
+                <span className="visually-hidden">Toggle weekday visibility</span>
               </ToggleButton>
             </div>
           )
