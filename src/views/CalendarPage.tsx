78699f1f7adf79de500787584a9759c10fb6a657
Paul K. Lynch <dramatichamster@gmail.com>
2026-01-07 13:40:32 +1100

    Clean up

--- src/views/CalendarPage.tsx
+++ src/views/CalendarPage.tsx
@@ -1,22 +1,11 @@
-// import { useParams } from 'react-router'
 import { Calendar } from 'features/calendar'
 import { Title } from 'features/title'
 import { Toolbar } from 'features/toolbar'
-// import { parseMonth, parseYear } from 'global/utils'
 import { MainWrapper } from './MainWrapper'
 
 import type { FC } from 'react'
 
-// type CalendarViewParams = Record<'year' | 'month', string>
-
 export const CalendarView: FC = () => {
-  // const {
-  //   month,
-  //   year
-  // } = useParams<CalendarViewParams>()
-  // const yearClean = parseYear(year)
-  // const monthClean = parseMonth(month)
-  // console.log({ month, year, monthClean, yearClean })
   return (
     <MainWrapper>
       <Title />
